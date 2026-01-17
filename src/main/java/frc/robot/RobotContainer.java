29f71a6821582fd201f320c67cd5d40750082c37
ByteForceRobotics <frc10302@gmail.com>
2026-01-17 14:59:31 -0500

    fixed/replaced deprecated methods

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -25,8 +25,7 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.Constants.ElevatorConstants;
 import frc.robot.Constants.OIConstants;
 import frc.robot.subsystems.DriveSubsystem;
-import frc.robot.subsystems.ElevatorSubsystem;
-import frc.robot.subsystems.ReefSubsystem;
+import frc.robot.subsystems.ClimbSubsystem;
 import frc.robot.subsystems.VisionSubsystem;
 
 /*
@@ -40,14 +39,13 @@ public class RobotContainer {
   // The robot's subsystems
   private final SendableChooser<Command> autoChooser;
   private final DriveSubsystem m_robotDrive = new DriveSubsystem();
-  private final ElevatorSubsystem m_elevator = new ElevatorSubsystem();
-  private final ReefSubsystem m_reef = new ReefSubsystem();
+  private final ClimbSubsystem m_climber = new ClimbSubsystem();
   private final VisionSubsystem m_vision = new VisionSubsystem();
   
 
   // The driver's controller
   XboxController m_driverController = new XboxController(OIConstants.kDriverControllerPort);
-  XboxController m_elevatorController = new XboxController(OIConstants.kElevatorControllerPort);
+  XboxController m_climberController = new XboxController(OIConstants.kElevatorControllerPort);
   //CommandXboxController m_commanddriverController = new CommandXboxController(OIConstants.kDriverControllerPort);
   
   
@@ -67,14 +65,11 @@ public class RobotContainer {
   double speedScaleHigh = 1.0;
   double speedScaleLow= 0.33;
   double speedScale = speedScaleHigh;
-  SequentialCommandGroup deployCoralCommmand= new RunCommand(
-        () -> m_reef.moveCoral(0.165),
-        m_elevator).withTimeout(1.0).andThen(new InstantCommand(
-          () -> m_reef.moveCoral_stop(),m_elevator));
   public RobotContainer() {
     // Configure the button bindings
     configureButtonBindings();
-    //CameraServer.startAutomaticCapture();
+    
+    autoChooser = AutoBuilder.buildAutoChooser();
     
     // Configure default commands
     m_robotDrive.setDefaultCommand(
@@ -88,25 +83,6 @@ public class RobotContainer {
             -MathUtil.applyDeadband(m_driverController.getRightX()*speedScale, OIConstants.kDriveDeadband),
             fieldRelative),
         m_robotDrive));
-
-    m_reef.setDefaultCommand(
-        new RunCommand(
-            () -> m_reef.autoLoadCoral(),
-            m_reef)
-    );
-    /*
-    m_elevator.setDefaultCommand(
-        new RunCommand(
-            () -> m_elevator.stayAtPosition(),
-            m_elevator)
-    );
-    */
-    NamedCommands.registerCommand("LiftElevatorCoral4", new RunCommand(
-        () -> m_elevator.goToPosition(-35),//is inverted because upwards is negative
-        m_elevator));
-        autoChooser = AutoBuilder.buildAutoChooser();
-    
-    NamedCommands.registerCommand("DeployCoral", deployCoralCommmand);
     
 
     // Another option that allows you to specify the default auto by its name
@@ -119,44 +95,7 @@ public class RobotContainer {
   }
   
   public void periodic() {
-    /* 
-   
-
-    if(Math.abs(m_elevator.getElevatorPosition())>5){
-        speedScale = 0.1;
-    }
-    if(Math.abs(m_elevator.getElevatorPosition())>80&&
-    Math.abs(m_robotDrive.getSpeed())>0.2){
-      m_elevator.goToPosition(0);
-    }
-    if(Math.abs(m_robotDrive.getSpeed())>1){
-      m_elevator.goToPosition(0);
-    }
-    */
     
-    //boolean hasTargets = result.hasTargets();
-    // java -jar "C:\FRC Code\PhotonVision\photonvision-v2025.3.1-winx64.jar"
-    
-    /* 
-    for (var target : result.getTargets()) {
-      targetId = target.getFiducialId();
-      
-      if (target.getFiducialId() == 22) {
-        
-          // Found Tag 7, record its information
-          double targetYaw = target.getYaw();
-          double targetRange =
-          PhotonUtils.calculateDistanceToTargetMeters(
-            0.5, // Measured with a tape measure, or in CAD.
-            1.435, // From 2024 game manual for ID 7
-            Math.toRadians(-30.0), // Measured with a protractor, or in CAD.
-            Math.toRadians(target.getPitch()));
-
-          //targetVisible = true;
-      }
-          
-    }
-    */
   }
 
   public static Trigger triggerButton (XboxController controller, XboxController.Axis axis) {
@@ -171,88 +110,64 @@ public class RobotContainer {
    */
   private void configureButtonBindings() {
     /* 
-    new JoystickButton(m_elevatorController, Button.kStart.value)
+    new JoystickButton(m_climberController, Button.kStart.value)
     .onTrue(new RunCommand(
       () -> m_reef.moveCoral(0.165),
-      m_elevator).withTimeout(1.0).andThen(new InstantCommand(
+      m_climber).withTimeout(1.0).andThen(new InstantCommand(
         () -> m_reef.moveCoral_stop(),
-        m_elevator)));
+        m_climber)));
     */
     
-    new JoystickButton(m_elevatorController, Button.kStart.value)//sets current elevator pos to min
+    new JoystickButton(m_climberController, Button.kStart.value)//sets current elevator pos to min
         .whileTrue(new RunCommand(
-            () -> m_elevator.elevatorResetEncoders(),m_elevator));
+            () -> m_climber.elevatorResetEncoders(),m_climber));
     
     /*
-    new JoystickButton(m_elevatorController, Button.kBack.value)//toggles limits on and off
+    new JoystickButton(m_climberController, Button.kBack.value)//toggles limits on and off
         .onTrue(new InstantCommand(
-            () -> m_elevator.toggleElevatorFixMode(),m_elevator));
+            () -> m_climber.toggleElevatorFixMode(),m_climber));
     */
-
-    new JoystickButton(m_elevatorController, Button.kLeftBumper.value)
+    /* 
+    old bumper keybinds
+    new JoystickButton(m_climberController, Button.kLeftBumper.value)
         .whileTrue(new RunCommand(
             () -> m_reef.moveCoral(-0.165),m_reef))//takes coral back in
             .onFalse(new InstantCommand(
                 () -> m_reef.moveCoral_stop(),m_reef));
 
-    new JoystickButton(m_elevatorController, Button.kRightBumper.value)
+    new JoystickButton(m_climberController, Button.kRightBumper.value)
         .whileTrue(new RunCommand(
             () -> m_reef.moveCoral(0.165),m_reef))//dispenses coral
             .onFalse(new InstantCommand(
                 () -> m_reef.moveCoral_stop(),m_reef));
-
+    */
     
-    new JoystickButton(m_elevatorController, Button.kB.value)
+    new JoystickButton(m_climberController, Button.kB.value)
         .whileTrue(new RunCommand(
-            () -> m_elevator.goToPosition(-ElevatorConstants.kLevelTwoCoralHeight),//goes to level 2
-            m_elevator)).onFalse(new InstantCommand(
-                () -> m_elevator.lift_stop(),
-                m_elevator));
+            () -> m_climber.goToPosition(-ElevatorConstants.kLevelTwoCoralHeight),//goes to level 2
+            m_climber)).onFalse(new InstantCommand(
+                () -> m_climber.climb_stop(),
+                m_climber));
 
-    new JoystickButton(m_elevatorController, Button.kX.value)
+    new JoystickButton(m_climberController, Button.kA.value)
         .whileTrue(new RunCommand(
-            () -> m_elevator.goToPosition(-ElevatorConstants.kLevelThreeCoralHeight),//goes to level 3
-            m_elevator)).onFalse(new InstantCommand(
-                () -> m_elevator.lift_stop(),
-                m_elevator));
-
-    new JoystickButton(m_elevatorController, Button.kY.value)
-        .whileTrue(new RunCommand(
-            () -> m_elevator.goToPosition(-ElevatorConstants.maxNeededElevatorHeight),//goes to level 4
-            m_elevator)).onFalse(new InstantCommand(
-                () -> m_elevator.lift_stop(),
-                m_elevator));
-
-    new JoystickButton(m_elevatorController, Button.kA.value)
-        .whileTrue(new RunCommand(
-            () -> m_elevator.goToPosition(0),//goes to zdero
-            m_elevator)).onFalse(new InstantCommand(
-                () -> m_elevator.lift_stop(),
-                m_elevator));
-    
-    triggerButton(m_elevatorController,Axis.kLeftTrigger).whileTrue(new RunCommand(
-        () -> m_elevator.lift(m_elevatorController.getRawAxis(Axis.kLeftTrigger.value)),
-        m_elevator)).onFalse(new InstantCommand(
-            () -> m_elevator.lift_stop(),
-            m_elevator));
-
-    triggerButton(m_elevatorController,Axis.kRightTrigger).whileTrue(new RunCommand(
-        () -> m_elevator.lift(-m_elevatorController.getRawAxis(Axis.kRightTrigger.value)),
-        m_elevator)).onFalse(new InstantCommand(
-            () -> m_elevator.lift_stop(),
-            m_elevator));
+            () -> m_climber.goToPosition(0),//lowers all the way, need to figure out unit amount
+            m_climber)).onFalse(new InstantCommand(
+                () -> m_climber.climb_stop(),
+                m_climber));
     /*
-    new POVButton(m_elevatorController, 0)
-        .whileTrue(new RunCommand(
-            () -> m_reef.moveAlgae(-0.15),m_reef))//dispenses  algae
-            .onFalse(new InstantCommand(
-                () -> m_reef.moveAlgae_stop(),m_reef));
+    trigger buttons, might be useful
+    triggerButton(m_climberController,Axis.kLeftTrigger).whileTrue(new RunCommand(
+        () -> m_climber.climb(m_climberController.getRawAxis(Axis.kLeftTrigger.value)),
+        m_climber)).onFalse(new InstantCommand(
+            () -> m_climber.climb_stop(),
+            m_climber));
 
-    new POVButton(m_elevatorController, 180)
-    .whileTrue(new RunCommand(
-      () -> m_reef.moveAlgae(0.15),m_reef))//intakes algae
-      .onFalse(new InstantCommand(
-          () -> m_reef.moveAlgae_hold(),m_reef));
+    triggerButton(m_climberController,Axis.kRightTrigger).whileTrue(new RunCommand(
+        () -> m_climber.climb(-m_climberController.getRawAxis(Axis.kRightTrigger.value)),
+        m_climber)).onFalse(new InstantCommand(
+            () -> m_climber.climb_stop(),
+            m_climber));
     */
 
 
